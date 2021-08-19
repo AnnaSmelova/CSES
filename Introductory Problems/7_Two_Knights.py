c79be5e3caca37b0c93f259b15289f6666bdc8b9
Anna Smelova <a_anna_a@inbox.ru>
2021-08-20 00:32:04 +0300

    Solution of the seventh problem

---
+++ Introductory Problems/7_Two_Knights.py
@@ -0,0 +1,66 @@
+'''
+7. Two Knights
+Time limit: 1.00 s Memory limit: 512 MB
+Your task is to count for k=1,2,…,n the number of ways two knights can be placed on a k×k chessboard
+so that they do not attack each other.
+
+Input
+
+The only input line contains an integer n.
+
+Output
+
+Print n integers: the results.
+
+Constraints
+1≤n≤10000
+Example
+
+Input:
+8
+
+Output:
+0
+6
+28
+96
+252
+550
+1056
+1848
+Introductory Problems
+...
+Increasing Array
+Permutations
+Number Spiral
+Two Knights
+Two Sets
+Bit Strings
+Trailing Zeros
+Coin Piles
+...
+Your submissions
+'''
+
+
+def get_bad_points_count(n):
+    if n == 1:
+        return 0
+    elif n == 2:
+        return 6
+    elif n == 3:
+        return 28
+    elif n == 4:
+        return 96
+    else:
+        return int((n**4 - n**2 - 8 * (n - 4)**2 - 40 * (n - 4) - 48) / 2)
+
+
+def main():
+    n = int(input())
+    for k in range(1, n + 1):
+        print(get_bad_points_count(k))
+
+
+main()
+
